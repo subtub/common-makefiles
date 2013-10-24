45f279526727e0c65ddfbc091cb89e291dc03426
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-24 12:09:09 +0200

    Update build and test script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,9 @@
-node_modules/.bin/rci --input src/index --output index.make
-node_modules/.bin/rci --input src/subtool --output subtool.make
+node_modules/.bin/rci --input src/makefile_base/index   --output index.make
+
+node_modules/.bin/rci --input src/subtool               --output subtool.make
+
+node_modules/.bin/rci --input src/node/all              --output node.make
+node_modules/.bin/rci --input src/node/jsdox            --output jsdox.make
+node_modules/.bin/rci --input src/node/jshint           --output jshint.make
+node_modules/.bin/rci --input src/node/mocha            --output mocha.make
+node_modules/.bin/rci --input src/node/plato            --output plato.make
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,12 @@
-echo "Test build.sh"
+echo "--> Test Building"
 sh scripts/build.sh
+
+echo "--> Test Makefiles"
+echo "    jsdox.make"
+make -f jsdox.make jsdox-help
+echo "    jshint.make"
+make -f jshint.make jshint-help
+echo "    mocha.make"
+make -f mocha.make mocha-help
+echo "    plato.make"
+make -f plato.make plato-help
