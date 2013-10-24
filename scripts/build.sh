c2c689a030bd7f527ea5332d18a60d7d9e7259aa
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-24 12:22:32 +0200

    delete index file from build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,3 @@
-node_modules/.bin/rci --input src/makefile_base/index   --output index.make
-
 node_modules/.bin/rci --input src/subtool               --output subtool.make
 
 node_modules/.bin/rci --input src/node/all              --output node.make
