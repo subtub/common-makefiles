daf5b872b06e0de74e772b20e9acc45dab5b0268
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-24 17:17:47 +0200

    Changed generate structure

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,13 @@
-node_modules/.bin/rci --input src/subtool               --output subtool.make
+###
+# Build the subtool Makefile
+###
+node_modules/.bin/rci   --input src/subtool   --output subtool.make
 
-node_modules/.bin/rci --input src/node/all              --output node.make
-node_modules/.bin/rci --input src/node/jsdox            --output jsdox.make
-node_modules/.bin/rci --input src/node/jshint           --output jshint.make
-node_modules/.bin/rci --input src/node/mocha            --output mocha.make
-node_modules/.bin/rci --input src/node/plato            --output plato.make
+###
+# Build the node Makefiles
+###
+node_modules/.bin/rci   --input src/node/all      --output node.make
+node_modules/.bin/rci   --input src/node/jsdox    --output jsdox.make
+node_modules/.bin/rci   --input src/node/jshint   --output jshint.make
+node_modules/.bin/rci   --input src/node/mocha    --output mocha.make
+node_modules/.bin/rci   --input src/node/plato    --output plato.make
