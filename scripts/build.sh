39c06813de5b24f4121d514ad14c3ebbc36f5e5e
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-25 09:27:53 +0200

    Update RecursiveContentInclude (now at npm)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 ###
 # Build the subtool Makefile
 ###
-node_modules/.bin/rci   --input src/subtool   --output subtool.make
+node_modules/.bin/resi   --input src/subtool   --output subtool.make
 
 ###
 # Build the node Makefiles
 ###
-node_modules/.bin/rci   --input src/node/all      --output node.make
-node_modules/.bin/rci   --input src/node/jsdox    --output jsdox.make
-node_modules/.bin/rci   --input src/node/jshint   --output jshint.make
-node_modules/.bin/rci   --input src/node/mocha    --output mocha.make
-node_modules/.bin/rci   --input src/node/plato    --output plato.make
+node_modules/.bin/resi   --input src/node/all      --output node.make
+node_modules/.bin/resi   --input src/node/jsdox    --output jsdox.make
+node_modules/.bin/resi   --input src/node/jshint   --output jshint.make
+node_modules/.bin/resi   --input src/node/mocha    --output mocha.make
+node_modules/.bin/resi   --input src/node/plato    --output plato.make
