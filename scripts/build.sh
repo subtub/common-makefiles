bbf8b54a466437dfa08118f57495a39249643dd4
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-11-05 10:16:24 +0100

    Update build.sh (added apidoc)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,13 +4,21 @@
 # This files were published at npm registry.
 #
 ###
-node_modules/.bin/resi   --input src/subtool   --output subtool.make
 
-###
-# Build the node Makefiles
-###
-node_modules/.bin/resi   --input src/node/all      --output node.make
-node_modules/.bin/resi   --input src/node/jsdox    --output jsdox.make
-node_modules/.bin/resi   --input src/node/jshint   --output jshint.make
-node_modules/.bin/resi   --input src/node/mocha    --output mocha.make
-node_modules/.bin/resi   --input src/node/plato    --output plato.make
+
+echo "Build subtool.make"
+node_modules/.bin/resi --input src/subtool --output subtool.make
+
+echo "Build node.make"
+node_modules/.bin/resi --input src/node/all --output node.make
+
+echo "Build apidoc.make"
+node_modules/.bin/resi --input src/node/apidoc --output apidoc.make
+echo "Build jsdox.make"
+node_modules/.bin/resi --input src/node/jsdox --output jsdox.make
+echo "Build jshint.make"
+node_modules/.bin/resi --input src/node/jshint --output jshint.make
+echo "Build mocha.make"
+node_modules/.bin/resi --input src/node/mocha --output mocha.make
+echo "Build plato.make"
+node_modules/.bin/resi --input src/node/plato --output plato.make
