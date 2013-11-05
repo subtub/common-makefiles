050e9de35c325ef2a499d341375039f6048fdaea
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-11-05 12:38:42 +0100

    Update paths

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,18 +7,18 @@
 
 
 echo "Build subtool.make"
-node_modules/.bin/resi --input src/subtool --output subtool.make
+node_modules/.bin/resi --input src/subtool/index --output subtool.make
 
 echo "Build node.make"
 node_modules/.bin/resi --input src/node/all --output node.make
 
 echo "Build apidoc.make"
-node_modules/.bin/resi --input src/node/apidoc --output apidoc.make
+node_modules/.bin/resi --input src/node/apidoc/index --output apidoc.make
 echo "Build jsdox.make"
-node_modules/.bin/resi --input src/node/jsdox --output jsdox.make
+node_modules/.bin/resi --input src/node/jsdox/index --output jsdox.make
 echo "Build jshint.make"
-node_modules/.bin/resi --input src/node/jshint --output jshint.make
+node_modules/.bin/resi --input src/node/jshint/index --output jshint.make
 echo "Build mocha.make"
-node_modules/.bin/resi --input src/node/mocha --output mocha.make
+node_modules/.bin/resi --input src/node/mocha/index --output mocha.make
 echo "Build plato.make"
-node_modules/.bin/resi --input src/node/plato --output plato.make
+node_modules/.bin/resi --input src/node/plato/index --output plato.make
